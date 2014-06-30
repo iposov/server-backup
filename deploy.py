e5c446af6e2d19de1a3edb3e47a9dc3caad78e10
ilya <iposov@gmail.com>
2014-06-30 13:49:10 +0400

    instantly redirect commands output to stderr, print deployment start, finish, passed time

--- deploy.py
+++ deploy.py
@@ -2,6 +2,7 @@
 import yaml
 import os
 import sys
+from datetime import datetime
 from subprocess import Popen, PIPE
 
 
@@ -10,10 +11,17 @@ def hookprint(msg):
     print >> sys.stderr, msg
 
 
-def command(cmd, inp=None, wd=None):
-    child = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE, cwd=wd)
+def command(cmd, inp=None, wd=None, direct_output=False):
+    if direct_output:
+        child = Popen(cmd, shell=True, stdout=sys.stderr, cwd=wd)
+    else:
+        child = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE, cwd=wd)
     out, err = child.communicate(inp)
-    return out, err, child.returncode
+
+    if direct_output:
+        return child.returncode
+    else:
+        return out, err, child.returncode
 
 
 def last_deploy_tag_name(branch_name):
@@ -22,10 +30,9 @@ def last_deploy_tag_name(branch_name):
 
 def get_changeset_from_log(log):
     log = log.partition('\n')[0]
-    log = log[len('changeset:') :]
+    log = log.split(':')[2]
     log = log.strip()
-    log = log.partition(':')
-    return log[0]
+    return log
 
 
 def get_branch_change(branch_name):
@@ -52,9 +59,9 @@ def do_sync(changeset_from, changeset_to, location_src, location_dst, need_delet
     if need_delete:
         delete_arg = '--delete '
     else:
-        delete_ard = ''
+        delete_arg = ''
 
-    rsync_command = 'rsync -az {0}--exclude=.hg {1}/ {2}'.format(location_src, location_dst)
+    rsync_command = 'rsync -az {0}--exclude=.hg {1}/ {2}'.format(delete_arg, location_src, location_dst)
     _, err, code = command(rsync_command)
     if code:
         hookprint('Failed to sync {0} and {1}'.format(location_src, location_dst))
@@ -69,12 +76,8 @@ def do_command(changeset_from, changeset_to, server, cmd, wd):
         #TODO properly escape a command
         cmd = 'ssh {0} "{1}"'.format(server, cmd)
 
-    out, err, code = command(cmd, wd=wd)
+    code = command(cmd, wd=wd, direct_output=True)
     hookprint('exit code {0}'.format(code))
-    if out:
-        hookprint('STD OUT: {0}'.format(out))
-    if err:
-        hookprint('STD ERR: {0}'.format(err))
 
 
 def do_actions(changeset_from, changeset_to, actions):
@@ -82,7 +85,7 @@ def do_actions(changeset_from, changeset_to, actions):
         if 'sync' in action:
             do_sync(changeset_from, changeset_to, action['source'], action['dest'], action.get('delete', False))
         elif 'command' in action:
-            do_command(changeset_from, changeset_to, action.get('where', '$local'), action['command'], action.get('wd', False))
+            do_command(changeset_from, changeset_to, action.get('where', '$local'), action['command'], action.get('wd', None))
         else:
             hookprint('Unknown action "{0}"'.format(action))
             sys.exit(1)
@@ -101,6 +104,11 @@ def update_repo(changeset):
 
 
 def do_deploy(repo_folder=False, repo_branch=False):
+    start_time = datetime.now()
+    hookprint("-------------------------------------------------------------------------------------------")
+    hookprint("                 Start deploy on %s" % str(start_time))
+    hookprint("-------------------------------------------------------------------------------------------")
+
     #load config and search for actions
     if not repo_folder:
         repo_folder = os.getcwd()
@@ -131,6 +139,12 @@ def do_deploy(repo_folder=False, repo_branch=False):
             _, err, code = command('hg bookmark -fr {0} {1}'.format(branch_changeset, last_deploy_tag_name(branch_name)))
         else:
             hookprint('Branch {0} does not need to be redeployed'.format(branch_name))
+
+    stop_time = datetime.now()
+    hookprint("-------------------------------------------------------------------------------------------")
+    hookprint("                 Finished deploy on %s" % str(stop_time))
+    hookprint("                 Passed %s" % str(stop_time - start_time))
+    hookprint("-------------------------------------------------------------------------------------------")
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
