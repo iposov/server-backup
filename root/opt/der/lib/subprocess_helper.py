8b6fea124c9e366d08ca253b5010ff9cd126946b
Ilya Posov <iposov@gmail.com>
2017-11-28 11:20:08 +0300

    fix escaping arguments in subprocess helpers

--- root/opt/der/lib/subprocess_helper.py
+++ root/opt/der/lib/subprocess_helper.py
@@ -3,29 +3,32 @@ import subprocess
 
 def call(args_list, logger=None):
     def escape_argument(arg):
-        return "'" + arg.replace("'", "'\''") + "'"
+        return "'" + arg.replace("'", "'\\''") + "'"
 
     command_line_args = []
     command_line_args_to_print = []
 
     for arg_info in args_list:
         print_arg = True
+        escape_arg = True
 
         if isinstance(arg_info, list):
             arg = arg_info[0]
             for i in xrange(1, len(arg_info)):
                 command = arg_info[i]
                 if command == 'as-is':
-                    arg = escape_argument(arg)
+                    escape_arg = False
                 elif command == 'secret':
                     print_arg = False
         else:
             arg = arg_info
 
+        if escape_arg:
+            arg = escape_argument(arg)
+
         command_line_args.append(arg)
         command_line_args_to_print.append(arg if print_arg else '[[secret]]')
 
-    command_line_args = [arg[0] if isinstance(arg, list) else escape_argument(arg) for arg in args_list]
     # TODO use Popen constructor and communicate to get stderr/stdout
     command_line = " ".join(command_line_args)
 
